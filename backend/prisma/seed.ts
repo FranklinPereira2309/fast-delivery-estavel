b1f5b67f13db79f8275cfb05aaba251db24f7d1a
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2026-02-19 19:57:03 -0300

    Ajuste do Super usuário

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+  const adminEmail = 'admin@admin.com'
+  
+  const admin = await prisma.user.upsert({
+    where: { email: adminEmail },
+    update: {},
+    create: {
+      email: adminEmail,
+      name: 'Administrador Master',
+      password: 'admin', // Altere após o primeiro acesso
+      permissions: [
+        'dashboard',
+        'pos',
+        'sales-monitor',
+        'tables',
+        'kitchen',
+        'crm',
+        'inventory',
+        'logistics',
+        'settings'
+      ]
+    },
+  })
+
+  console.log({ admin })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
