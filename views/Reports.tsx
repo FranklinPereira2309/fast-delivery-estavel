67f1307d5012631a2713defa0135b1315a2cb2b9
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2026-02-22 10:10:15 -0300

    Troca de título em relatório de relação de entregadores

--- views/Reports.tsx
+++ views/Reports.tsx
@@ -328,7 +328,7 @@ const Reports: React.FC = () => {
             page.drawText('DATA e HORA', { x: 55, y, size: 8, font: fontBold });
             page.drawText('CLIENTE', { x: 180, y, size: 8, font: fontBold });
             page.drawText('ENTREGADOR', { x: 350, y, size: 8, font: fontBold });
-            page.drawText('VALOR TOTAL', { x: 500, y, size: 8, font: fontBold });
+            page.drawText('TOTAL', { x: 500, y, size: 8, font: fontBold });
             y -= 25;
 
             for (const o of filteredOrders) {
