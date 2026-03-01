ef1e10bb416a9a5aa56329df0bc3acce40467e9a
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2026-03-01 10:20:20 -0300

    fix:botão nfce em pagto

--- views/POS.tsx
+++ views/POS.tsx
@@ -1183,7 +1183,7 @@ const POS: React.FC<POSProps> = ({ currentUser }) => {
             </div>
 
             <div className={`p-4 lg:p-6 bg-slate-50 border-t border-slate-100 shrink-0 flex flex-col gap-2`}>
-              {!!isReceivingFiado && (
+              {paymentMethod !== 'FIADO' && (
                 <div className="flex items-center justify-between px-4 py-2 bg-white rounded-2xl border border-slate-100">
                   <div className="flex items-center gap-3">
                     <Icons.View className="w-5 h-5 text-blue-600" />
@@ -2783,7 +2783,7 @@ const POS: React.FC<POSProps> = ({ currentUser }) => {
               {/* Modal Footer Controls */}
               <div className="p-3 lg:p-4 border-t border-slate-200 bg-white rounded-b-[2rem] shrink-0 flex items-center justify-between">
                 <div>
-                  {!!isReceivingFiado && (
+                  {paymentMethod !== 'FIADO' && paymentMethod2 !== 'FIADO' && (
                     <div className="flex items-center gap-2 bg-slate-100 px-3 py-1.5 rounded-lg">
                       <Icons.View className="w-4 h-4 text-slate-500" />
                       <div>
