8c76201c82075ecf6d1f9cc63ae24145c366a925
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2026-03-03 16:26:43 -0300

    style: trocar ícone do módulo de cozinha para um modelo de chapéu de chef

--- constants.tsx
+++ constants.tsx
@@ -41,7 +41,7 @@ export const Icons = {
   ),
   Kitchen: (props: any) => (
     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}>
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18h12v2a2 2 0 01-2 2H8a2 2 0 01-2-2v-2zm12-4a6 6 0 10-12 0v2h12v-2zM9 8a3 3 0 116 0 3 3 0 01-6 0z" />
     </svg>
   ),
   POS: (props: any) => (
